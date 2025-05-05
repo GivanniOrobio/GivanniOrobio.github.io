b1043b086da85aa980c6b5c42477aa1596016a1f
giovanny <giorca7410@gmail.com>
2025-05-05 03:07:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php 
-// require_once "controllers/web.php";
-// require_once "controllers/control.php";
-// require_once "controllers/auth.php";
+require_once "controllers/web.php";
+require_once "controllers/control.php";
+require_once "controllers/auth.php";
 
-// require_once "models/web.php";
-// require_once "models/auth.php";
-// require_once "models/web.php";
-// $template = new Control();
-// $template->template();
+require_once "models/web.php";
+require_once "models/auth.php";
+require_once "models/web.php";
+$template = new Control();
+$template->template();
 echo ("test");
 ?>
