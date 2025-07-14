08625a7a349cffd7590f974799b793be78ce1fde
giovanny <giorca7410@gmail.com>
2025-07-13 23:00:32 -0500

    primer commit

--- Http/home/navegations/userMenu.php
+++
@@ -1,9 +0,0 @@
-<ul class="header_user">
-
-    <li><a href="index.php?action=account/login" id="customer_login_link">Log in</a></li>
-
-    <li><a href="index.php?action=account/register" id="customer_register_link">Create an account</a></li>
-
-
-    <li class="checkout"><a href="index.php?action=cart">Check out</a></li>
-</ul>
--- controllers/control.php
+++
@@ -1,8 +0,0 @@
-<?php 
-class Control{
-    static public function template(){
-        include "views/template.php");
-    }
-}
-
-?>
